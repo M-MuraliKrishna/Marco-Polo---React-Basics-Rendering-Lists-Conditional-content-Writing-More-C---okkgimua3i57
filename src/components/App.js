118fa2f2abc0ac10623e27aed1dd3730a7574aa3
Murali Krishna M <murali24011999@gmail.com>
2023-04-02 06:50:06 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  
+  const [text, setText] = useState(true);
+  const handleChange = () => {
+    setText(!text);
+  };
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{text ? "Marco" : "Polo"}</h1>
+      <button id="marco-polo-toggler" onClick={handleChange}>
+        {text ? "Polo" : "Marco"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
